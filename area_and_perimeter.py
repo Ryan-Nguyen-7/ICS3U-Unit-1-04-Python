4252c5c692c1dd8a3464810fa7c7c94a436a3fc1
Ryan-Nguyen-7 <ryan.nguyen@mths.ca>
2020-11-25 10:43:17 +0000

    added basic_math.py and area_and_perimeter.py

---
+++ area_and_perimeter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Nguyen
+# Created on November 2020
+# This program calclates area and perimaeter of a rectangle
+#   with dimensions 5cm x 3cm
+
+
+def main():
+    # this function calculates area and perimeter
+    print("Ifa rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(5*3))
+    print("Perimeter is {}cm".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Nguyen
+# Created on November 2020
+# This program does basic math
+
+
+def main():
+    # this function does basic math
+    print(9+2)
+    print("7-3={}".format(7-3))
+    print("4/2={}".format(4/2))
+    print("4+4/2={}".format(4+4/2))
+    print("3+4*2={}".format(3+4*2))
+    print("5+2**3={}".format(5+2**3))
+
+
+
+if __name__ == "__main__":
+    main()
